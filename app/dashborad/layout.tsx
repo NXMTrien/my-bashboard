7bcffff6404d1106876a9695f4504dd126f4b069
NXMTrien <minhtrien1322@gmail.com>
2025-10-07 11:36:18 +0700

    Craete navigation Between Pages.

---
+++ app/dashborad/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SideNav from "../ui/dashboard/sidenav";
+
+export default function Layout({chidren}:{chidren: React.ReactNode}){
+    return(
+
+        <div className="flex h-sreen flex-col md:flex-row flex-row md:overflow-hidden">
+            <div className="w-full flex-noen md:w-64">
+                <SideNav />
+            </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{chidren}</div>
+        </div>
+    );
+}
